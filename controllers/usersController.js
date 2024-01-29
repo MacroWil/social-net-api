cbec8f10d6df3f0c5170caaf953ce3efa67f4216
macrowil <macrow.co@gmail.com>
2024-01-29 15:57:08 -0700

    added routes for users, updated seed data, and added controllers for users.

---
+++ controllers/usersController.js
@@ -0,0 +1,100 @@
+const { Users, Thoughts } = require("../models");
+
+module.exports = {
+  async getUser(req, res) {
+    try {
+      const user = await Users.find({});
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async getSingleUser(req, res) {
+    try {
+      const user = await Users.findOne({ _id: req.params.userId })
+        .populate("thoughts")
+        .populate("friends")
+        .select("-__v");
+      if (!user) {
+        res.status(404).json({ message: "No such ID!" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async createUser(req, res) {
+    try {
+      const user = await Users.create(req.body);
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
+  async updateUser(req, res) {
+    try {
+      const user = await Users.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: "No such ID!" });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // also remove a user's thoughts when deleted.
+  async deleteUser(req, res) {
+    try {
+      const user = await Users.findOneAndDelete({ _id: req.params.userId });
+      if (!user) {
+        res.status(404).json({ message: "No such ID!" });
+      } else {
+        Thoughts.deleteMany({ _id: { $in: user.thoughts } });
+        res.json({ message: "User and Thoughts deleted!" });
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async addFriend(req, res) {
+    try {
+      const user = await Users.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.params.friendId } },
+        { runValidators: true, new: true }
+      );
+      if (!user) {
+        res.status(404).json({ message: "No such ID!" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async deleteFriend(req, res) {
+    try {
+      const user = await Users.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId } },
+        { new: true }
+      );
+      if (!user) {
+        res.status(404).json({ message: "No such ID!" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
--- utils/seed.js
+++ utils/seed.js
@@ -5,7 +5,6 @@ connection.on("error", (err) => err);
 
 connection.once("open", async () => {
   console.log("connected");
-  // Delete the collections if they exist
   let usersCheck = await connection.db
     .listCollections({ name: "users" })
     .toArray();
@@ -20,28 +19,70 @@ connection.once("open", async () => {
     await connection.dropCollection("thoughts");
   }
 
-  const Thoughts = [
+  const thoughtsData = [
     {
-      first,
-      last,
-      github,
-      assignments,
+      _id: "65b81e25ae9cb7d74f877b07",
+      thoughtText: "POTATOES",
+      username: "maincharacter",
+      createdAt: "2024-01-29T21:52:37.038Z",
+      reactions: [
+        {
+          reactionBody: "PO TA TOES, boil em mash em stick em in a stew",
+          username: "morgyman",
+          _id: "65b81fdbae9cb7d74f877b1a",
+          reactionId: "65b81fdbae9cb7d74f877b1b",
+          createdAt: "2024-01-29T21:59:55.937Z",
+        },
+      ],
+      __v: 0,
+      totalReactionS: 1,
     },
     {
-      first,
-      last,
-      github,
-      assignments,
+      _id: "65b820e9ae9cb7d74f877b29",
+      thoughtText: "True facts about morgan freeman!",
+      username: "morgyman",
+      createdAt: "2024-01-29T22:04:25.240Z",
+      reactions: [],
+      __v: 0,
+      totalReactionS: 0,
     },
   ];
 
-  // Add students to the collection and await the results
-  await Thoughts.insertMany(Thoughts);
-  await Users.insertMany(Users);
+  const usersData = [
+    {
+      _id: "65b81c09fe872543d31277b3",
+      username: "morgyman",
+      email: "notmorganfreeman@gmail.com",
+      thoughts: ["65b820e9ae9cb7d74f877b29"],
+      friends: [],
+      __v: 0,
+      friendCount: 0,
+    },
+    {
+      _id: "65b81ca681d28528d00d0654",
+      username: "bestman",
+      email: "samwisegamgee@gmail.com",
+      thoughts: [],
+      friends: ["65b81c09fe872543d31277b3"],
+      __v: 0,
+      friendCount: 1,
+    },
+    {
+      _id: "65b81d1081d28528d00d0661",
+      username: "maincharacter",
+      email: "samwisegamgee2@gmail.com",
+      thoughts: ["65b81e25ae9cb7d74f877b07"],
+      friends: [],
+      __v: 0,
+      friendCount: 0,
+    },
+  ];
 
-  // Log out the seed data to indicate what should appear in the database
-  console.table(Users);
-  console.table(Thoughts);
+  await Thoughts.insertMany(thoughtsData);
+  await Users.insertMany(usersData);
+
+  console.table(usersData);
+  console.table(thoughtsData);
   console.info("Seeding complete! 🌱");
   process.exit(0);
 });
